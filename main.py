ad2f4c92affabf6aea5d17abb5fd984b643cfe78
Bogdan Derdziak <bogdan.derdz@gmail.com>
2025-05-16 11:59:51 +0200

    opencv, brightness slider

---
+++ main.py
@@ -0,0 +1,41 @@
+import cv2
+from viewer import Viewer, BrightnessViewer
+
+
+def ex1():
+    img = cv2.imread("tekstura_grzybka.png")
+    cv2.imshow("", img)
+    cv2.waitKey(0)
+
+
+def ex2():
+    capture = cv2.VideoCapture(0)
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            break
+        cv2.imshow('', frame)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+def ex3():
+    cv2.namedWindow('window')
+    capture = cv2.VideoCapture(0)
+    cv2.createTrackbar('slider', 'window', 256, 256 * 2 - 1, lambda x: x)
+    while True:
+        ret, frame = capture.read()
+        if not ret:
+            break
+        cv2.imshow('window', cv2.add(frame, cv2.getTrackbarPos('slider', 'window') - 256))
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+
+def ex4():
+    viewer = BrightnessViewer()
+    viewer.run()
+
+
+if __name__ == '__main__':
+    ex4()
---
+++ viewer.py
@@ -0,0 +1,36 @@
+import cv2
+
+
+class Viewer:
+    def __init__(self, initial_value, max_value):
+        cv2.namedWindow('window')
+        self.capture = cv2.VideoCapture(0)
+        cv2.createTrackbar('slider', 'window', initial_value, max_value, lambda x: None)
+
+    def get_trackbar_pos(self):
+        return cv2.getTrackbarPos('slider', 'window')
+
+    def process_frame(self, frame, trackbar_pos):
+        return frame
+
+    def run(self):
+        while True:
+            ret, frame = self.capture.read()
+            if not ret:
+                break
+            trackbar_pos = self.get_trackbar_pos()
+            frame = self.process_frame(frame, trackbar_pos)
+            cv2.imshow('window', frame)
+            if cv2.waitKey(1) == ord('q'):
+                break
+
+
+class BrightnessViewer(Viewer):
+    def __init__(self):
+        super().__init__(256, 256 * 2 - 1)
+
+    def get_trackbar_pos(self):
+        return super().get_trackbar_pos() - 256
+
+    def process_frame(self, frame, trackbar_pos):
+        return cv2.add(frame, trackbar_pos)
